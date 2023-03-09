8b6699b54e6d57ecd50d6d22fff2870c304cff8b
bahadiryildirim <bahadir0yildirim@gmail.com>
2023-03-09 14:37:32 +0300

    Yannış yazmışsın

--- first.dart
+++ first.dart
@@ -1,6 +1,6 @@
 void main(List<String> args) {
   String first = "ilk satır kod";
   String second = "ikinci satır kod";
-  String third = "üçüncü satır kod";
+  String third = "üçüncü satmayır kod";
   String sa = "sa as";
 }
