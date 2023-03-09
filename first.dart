7416ec58ff6ca5b75cd00bd06fddd37f4dcdf9b9
bahadiryildirim <bahadir0yildirim@gmail.com>
2023-03-09 14:43:11 +0300

    maindekileri düzelttim

--- first.dart
+++ first.dart
@@ -1,6 +1,6 @@
 void main(List<String> args) {
   String first = "ilk satır kod";
-  String second = "ikinci satır kod";
+  String second = "ikinci satır koder mi";
   String third = "üçüncü satmayır kod";
   String sa = "sa as";
 }
