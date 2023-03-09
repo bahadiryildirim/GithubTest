0555ab58d238007991dfef96ace92bb1c562883d
bahadiryildirim <bahadir0yildirim@gmail.com>
2023-03-09 13:49:36 +0300

    toplama fonksiyonu

--- first.dart
+++ first.dart
@@ -2,4 +2,9 @@ void main(List<String> args) {
   String first = "ilk satır kod";
   String second = "ikinci satır kod";
   String third = "üçüncü satır kod";
+  topla(third, second, first);
+}
+
+void topla(third, second, first) {
+  String asd = first + second + third;
 }
